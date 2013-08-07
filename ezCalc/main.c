ba076149b4d46ac88db5f25506ec4d3254db9a36
keithGrout <keiwi.g@gmail.com>
2013-08-06 21:27:58 -0700

    Initial Commit

---
+++ ezCalc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ezCalc
+//
+//  Created by Keith on 8/6/13.
+//  Copyright (c) 2013 Appology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
